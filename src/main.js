8e59b7127231da71163505291677ccf7ba451f6b
Barış <baris@barisaksu.com>
2020-11-04 23:26:05 +0300

    Add tailwindcss

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
+import("@/assets/css/app.css");
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
