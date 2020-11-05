3b6498c9a39db6ae414b01cf531f87b5549d96ed
Barış <baris@barisaksu.com>
2020-11-05 12:54:01 +0300

    Add tonaljs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,12 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container mx-auto flex justify-center items-center h-screen">
+    <h1 class="text-2xl text-blue-400">Hello, Vivaldi!</h1>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+};
 </script>
